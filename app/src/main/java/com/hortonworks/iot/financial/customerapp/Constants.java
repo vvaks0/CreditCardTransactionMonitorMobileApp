ac2c4eaca63599f49276b2421ef891677887b0b6
vvaks <vvaks@hortonworks.com>
2016-05-10 09:49:45 -0400

    Initial

--- app/src/main/java/com/hortonworks/iot/financial/customerapp/Constants.java
+++ app/src/main/java/com/hortonworks/iot/financial/customerapp/Constants.java
@@ -4,8 +4,8 @@ package com.hortonworks.iot.financial.customerapp;
  * Created by vvaks on 3/19/16.
  */
 public class Constants {
-    public static String awsAccessKeyId = "AKIAJ72LVQNYTJ5CPL2A";
-    public static String awsSecretKey = "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";
-    public static String authorizedEntity = "646494417568";
-    public static String customerValidationQueueURL = "https://sqs.us-west-2.amazonaws.com/618900452667/customerValidation";
+    public static String awsAccessKeyId = "";
+    public static String awsSecretKey = "";
+    public static String authorizedEntity = "";
+    public static String customerValidationQueueURL = "";
 }
